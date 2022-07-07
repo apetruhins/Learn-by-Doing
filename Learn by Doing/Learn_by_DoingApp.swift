f93801b05ebe30bda280fcb560a8043baa79d76e
Aleksandrs Petruhins <aleksandrs.petruhins@gmail.com>
2022-07-07 23:04:57 +0300

    Initial commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Alex on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Learn by Doing
+Learn by Doing app from Udemy course SwiftUI Masterclass 2022 - iOS App Development &amp; Swift
